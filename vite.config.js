a4b0d8ff472384e2b06b60fd5404ba0f5a12f28d
李嵚峰 <qinfeng.lqf@gongdao.com>
2021-01-21 10:13:10 +0800

    feat: 更新配置

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,15 @@ export default {
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
+    },
+    build: {
+      // Template for index.html
+      index: path.resolve(__dirname, '../docs/index.html'),  //之前是'../dist/index.html'
+  
+      // Paths
+      assetsRoot: path.resolve(__dirname, '../docs'),  // 之前是 '../dist'
+      assetsSubDirectory: 'static',
+      assetsPublicPath: './',    // 之前是 '/'
     }
   }
   
