5a3319e0efeb4719097bc81fd70967f74e1736d0
李嵚峰 <qinfeng.lqf@gongdao.com>
2021-01-21 10:25:08 +0800

    feat: 更新配置

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,14 @@ export default {
         rewrite: path => path.replace(/^\/api/, '')
       }
     },
-    build: {
-      // Template for index.html
-      index: path.resolve(__dirname, '../docs/index.html'),  //之前是'../dist/index.html'
+    // build: {
+    //   // Template for index.html
+    //   index: path.resolve(__dirname, '../docs/index.html'),  //之前是'../dist/index.html'
   
-      // Paths
-      assetsRoot: path.resolve(__dirname, '../docs'),  // 之前是 '../dist'
-      assetsSubDirectory: 'static',
-      assetsPublicPath: './',    // 之前是 '/'
-    }
+    //   // Paths
+    //   assetsRoot: path.resolve(__dirname, '../docs'),  // 之前是 '../dist'
+    //   assetsSubDirectory: 'static',
+    //   assetsPublicPath: './',    // 之前是 '/'
+    // }
   }
   
